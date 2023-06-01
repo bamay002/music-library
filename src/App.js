d45c8f769fa59028f16633b5be49612637c7b991
Brenda <amaya.brendaa@gmail.com>
2023-06-01 14:22:05 -0700

    part 5 finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-import { useState, useRef } from 'react';
+import React, { useState, useRef, Fragment } from 'react';
 import Gallery from './Components/Gallery'
 import SearchBar from './Components/SearchBar'
 import { DataContext } from './Context/DataContext';
 import { SearchContext } from './Context/SearchContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AlbumView from './Views/AlbumView'
+import ArtistView from './Views/ArtistView'
 
 document.title = 'Music Library'
 
@@ -34,16 +37,26 @@ function App() {
 
   return (
     <div className="App">
-      <SearchContext.Provider value={{
-        term: searchInput,
-        handleSearch: handleSearch
-      }}>
-        <SearchBar />
-      </SearchContext.Provider>
-        {message}
-      <DataContext.Provider value={data}>
-        <Gallery />
-      </DataContext.Provider>
+      <Router>
+        <Routes>
+          <Route path='/' element={
+            <Fragment>
+              <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+              }}>
+                <SearchBar />
+              </SearchContext.Provider>
+                {message}
+              <DataContext.Provider value={data}>
+                <Gallery />
+              </DataContext.Provider>
+            </Fragment>
+          } />
+              <Route path='/album/:id' element={<AlbumView />} />
+              <Route path='/artist/:id' element={<ArtistView />} />
+        </Routes>
+      </Router>
 
     </div>
   );
